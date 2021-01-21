4a8b270dcfe676660a42a759fe73953068e461cc
Amit Kumar Mishra <amitmishra.rh@gmail.com>
2021-01-21 13:49:23 +0530

    Day6 of 100DaysofCode CSS Flexbox

--- 100daysofcode/Test.java
+++ 100daysofcode/Test.java
@@ -1,32 +1,49 @@
+/* package codechef; // don't place package name! */
+
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class GFG
- {
-    public static void main (String[] args)
-     {
-         Scanner sc=new Scanner(System.in);
-         int t=sc.nextInt();
-         sc.nextLine();
-         while(t-->0){
-             String str=sc.nextLine();
-             int[] arr=new int[127];
-             for(char ch:str.toCharArray()){
-                 int index=(int)ch;
-                 arr[index]++;
-             }
-             int sum=0;
-             for(int i:arr){
-                 sum+=i;
-             }
-             
-             
-             
-             double sqrt=Math.sqrt(sum);   
-             
-             System.out.println((sqrt - Math.floor(sqrt)) == 0?1:0);
-         }
-         
-     //code
-     }
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Test
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        
+        int[][] arr=new int[t][2];
+        int i=0;
+        while(i<t){
+            sc.nextLine();
+            arr[i][0]=sc.nextInt();
+            arr[i][1]=sc.nextInt();
+           
+            i++;
+        }
+        int a=0,b=0;
+        int aDiff=0,bDiff=0;
+        for(int[] ab:arr){
+            if(ab[0]>ab[1]){
+                a++;
+                b--;
+                int diff=Math.abs(ab[0]-ab[1]);
+                if(aDiff<diff)
+                aDiff=diff;
+            }
+            else if(ab[0]<ab[1]){
+                a--;
+                b++;
+                int diff=Math.abs(ab[0]-ab[1]);
+                if(bDiff<diff)
+                bDiff=diff;
+            }
+            
+        }
+        if(a>b)
+        System.out.println(1+" "+aDiff);
+        else
+        System.out.println(2+" "+bDiff);
+
+    }
 }
