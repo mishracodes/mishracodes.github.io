40b3e79ff2530135400e6de0c4c0051f1c4f36ab
Amit Kumar Mishra <amitmishra.rh@gmail.com>
2021-02-02 23:49:57 +0530

    Day15 of 100DaysOfCode ES6

---
+++ 100daysofcode/frontend/ES6/use-destructuring-assignment-to-assign-variables-from-objects.js
@@ -0,0 +1,8 @@
+const HIGH_TEMPERATURES = {
+  today: 77,
+  tomorrow: 80
+};
+
+// Only change code below this line
+const { today: highToday, tomorrow: highTomorrow } = HIGH_TEMPERATURES;
+// Only change code above this line
